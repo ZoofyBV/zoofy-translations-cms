355bce32750382d613735f1c7db4ed85bdc8683c
Nadeem Akhtar <nadeem@zoofy.nl>
2021-08-18 16:42:28 +0200

    Added CSV import for translations

--- api/translations/controllers/translations.js
+++ api/translations/controllers/translations.js
@@ -5,4 +5,69 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { Translate } = require('@google-cloud/translate').v2;
+const projectId = 'crafty-sound-323308';
+const csv = require('csv-parser');
+const fs = require('fs');
+
+module.exports = {
+
+    async translate(ctx) {
+        const translate = new Translate({ projectId });
+        const { text, target } = ctx.request.body;
+        const [translation] = await translate.translate(text, target);
+        console.log(`Text: ${text}`);
+        console.log(`Translation: ${translation}`);
+    },
+
+    async import(ctx) {
+
+        const { request: { body, files: { files } = {} } } = ctx;
+        let translations = [];
+
+        let updated = 0;
+        let created = 0;
+
+        if (files.type == 'text/csv') {
+            fs.createReadStream(files.path)
+                .pipe(csv())
+                .on('data', (data) => translations.push(data))
+                .on('end', () => {
+                    console.log('Done!');
+
+                });
+        }
+
+        setTimeout(() => {
+            translations.forEach(async (translation) => {
+                const existing = await strapi.query("translations").findOne({ 
+                        translation_key: translation.translation_key, 
+                        locale: translation.locale });
+
+                if (existing !== undefined && existing !== null) {
+                    updated++;
+                    console.log("Updated: " + updated);
+                    await strapi.services.translations.update({ id: existing.id }, {
+                        ...translation, 
+                        status: true
+                       });
+                } else {
+                    created++;
+                    console.log("Created: " + created);
+                    await strapi.services.translations.create({
+                        ...translation, 
+                        status: true
+                    });
+                }
+            });
+        }, 100);
+
+        ctx.send({
+            ok: true,
+            created,
+            updated,
+        });
+
+    }
+};
+
